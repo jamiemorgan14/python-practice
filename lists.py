5b37d4e76882af72a2dd385e5a72e226872007e9
Jamie <jmorgan3475@gmail.com>
2019-04-21 17:45:32 -0600

    basic list methods

--- lists.py
+++ lists.py
@@ -1,2 +1,5 @@
-friends = ["Justine", "Dexter", "Kiko", "Collin", "Corey"]
-print(friends[2:4])
+friends = ["Justine", "Dexter", "Kiko", "Kiko", "Collin", "Corey"]
+lucky_numbers = [12, 14, 19, 21, 22, 42]
+# friends.pop()
+lucky_numbers.reverse()
+print(lucky_numbers)
