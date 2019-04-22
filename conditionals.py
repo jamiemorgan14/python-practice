7cab9303e8bf4eb0d438e3b18e7c1f9ec08ba4fd
Jamie <jmorgan3475@gmail.com>
2019-04-21 20:36:28 -0600

    comparison operators

--- conditionals.py
+++ conditionals.py
@@ -1,11 +1,23 @@
-is_male = False
-is_tall = True
+# is_male = False
+# is_tall = True
 
-if is_male and is_tall:
-    print("You are tall male")
-elif is_male and not (is_tall):
-    print("You are a short male")
-elif not(is_male) and is_tall:
-    print("you are not a male but are tall")
-else:
-    print("You are either not male or not tall or neither")
+# if is_male and is_tall:
+#     print("You are tall male")
+# elif is_male and not (is_tall):
+#     print("You are a short male")
+# elif not(is_male) and is_tall:
+#     print("you are not a male but are tall")
+# else:
+#     print("You are either not male or not tall or neither")
+
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print(max_num(3, 40, 5))
