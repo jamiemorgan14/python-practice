3a0c1159077334cb453a217ffd534fa991a6c95d
Jamie <jmorgan3475@gmail.com>
2019-04-21 23:37:07 -0600

    inheritance

---
+++ Inheritance/ChineseChef.py
@@ -0,0 +1,10 @@
+from Chef import Chef
+
+
+class ChineseChef(Chef):
+
+    def make_steak(self):
+        print("The chef makes hibachi steak")
+
+    def make_fried_rice(self):
+        print("The chef makes fried rice")
---
+++ Inheritance/app.py
@@ -0,0 +1,14 @@
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+myChef = Chef()
+
+
+# myChef.make_chicken()
+# myChef.make_salad()
+# myChef.make_steak()
+
+myChineseChef = ChineseChef()
+
+myChineseChef.make_steak()
+myChef.make_steak()
